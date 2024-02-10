cf151269b3a3cdc179b33d6bb435ee414a0a74a7
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-10 16:46:26 -0600

    :wrench: Configure initial VitePWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { ConfigEnv, defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const isProduction = mode === "production";
+  return {
+    plugins: [react(), VitePWA()],
+    publicDir: "public",
+    build: {
+      sourcemap: !isProduction,
+      outDir: "dist",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
