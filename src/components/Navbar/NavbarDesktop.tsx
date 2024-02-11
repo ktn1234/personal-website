1bc34c0f26e98d2da97052f5515e22068080d8cf
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-11 05:32:38 -0600

    :sparkles: :iphone: Created the Navbar for Desktop (large screens)

---
+++ src/components/Navbar/NavbarDesktop.tsx
@@ -0,0 +1,46 @@
+interface Link {
+  title: string;
+  url: string;
+}
+
+function NavbarDesktop(): JSX.Element {
+  const links: Link[] = [
+    {
+      title: "About",
+      url: "#about",
+    },
+    {
+      title: "Experience",
+      url: "#experience",
+    },
+    // {
+    //   title: "Projects",
+    //   url: "#projects",
+    // },
+    {
+      title: "Technologies",
+      url: "#technologies",
+    },
+  ];
+
+  return (
+    <nav className="pl-8 pr-8 pt-5 flex justify-between items-center">
+      <div>
+        <span className="cursor-pointer">
+          <p className="text-xl inline"> {"> kevdev"}</p>
+        </span>
+      </div>
+      <div>
+        <ul className="flex">
+          {links.map((link: Link, index: number) => (
+            <li className="pl-5" key={index}>
+              <a href={link.url}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavbarDesktop;
