5ff7f4ff01ec2b41684a4484a0e4c53b41989fb9
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-11 05:34:52 -0600

    :sparkles: Created SocialLinks component

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,53 @@
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillSuitcaseLgFill } from "react-icons/bs";
+
+interface SocialLink {
+  title: string;
+  url: string;
+  icon: JSX.Element;
+}
+
+// Icon color uses the theme's theme.colors.tertiary value in tail
+const socialLinks: SocialLink[] = [
+  {
+    title: "Github",
+    url: "https://github.com/ktn1234",
+    icon: <FaGithub color="#3AD305" cursor="pointer" size={22} />,
+  },
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/kevin-t-nguyen/",
+    icon: <FaLinkedinIn color="#3AD305" cursor="pointer" size={22} />,
+  },
+  {
+    title: "Email",
+    url: "mailto:ktn1234@yahoo.com",
+    icon: <HiOutlineMail color="#3AD305" cursor="pointer" size={22} />,
+  },
+  {
+    title: "Resume",
+    url: "https://drive.google.com/file/d/1qRxhm3iu8-s4G1b3-USHA8lGYR9U9xq8/view?usp=sharing&usp=embed_facebook",
+    icon: <BsFillSuitcaseLgFill color="#3AD305" cursor="pointer" size={22} />,
+  },
+];
+
+function SocialLinks(): JSX.Element {
+  return (
+    <div className="flex justify-center items-center">
+      {socialLinks.map((link: SocialLink, index: number) => (
+        <a
+          key={index}
+          href={link.url}
+          target="_blank"
+          rel="noreferrer"
+          className="pr-5"
+        >
+          {link.icon}
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export default SocialLinks;
