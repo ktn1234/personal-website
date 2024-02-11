9f8bdf94b5c16080f36cd7bd1d86c183ccd361de
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-11 05:38:48 -0600

    :sparkles: :iphone: Created Technologies section

---
+++ src/sections/Technologies.tsx
@@ -0,0 +1,75 @@
+interface TechDomain {
+  name: string;
+  tech: string[];
+}
+
+const techDomains: TechDomain[] = [
+  {
+    name: "Languages",
+    tech: [
+      "Typescript",
+      "Go",
+      "Node.js",
+      "Python",
+      "Shell",
+      "Javascript",
+      "C++",
+    ],
+  },
+  {
+    name: "Web Frameworks",
+    tech: ["React", "Vue", "Next.js", "Express.js"],
+  },
+  {
+    name: "Web",
+    tech: [
+      "HTML",
+      "CSS",
+      "TailwindCSS",
+      "MaterialUI",
+      "REST",
+      "GraphQL",
+      "gRPC",
+    ],
+  },
+  {
+    name: "DevOps",
+    tech: ["Github Actions", "Jenkins", "Ansible"],
+  },
+  {
+    name: "Cloud",
+    tech: ["AWS", "GCP", "Azure", "Kubernetes", "Docker", "Terraform"],
+  },
+];
+
+function Technologies(): JSX.Element {
+  return (
+    <div>
+      <h1
+        id="technologies"
+        className="text-3xl text-center drop-shadow-tertiary-glow"
+      >
+        Technologies
+      </h1>
+      <div className="flex flex-wrap justify-between pt-5">
+        {techDomains.map((techDomain: TechDomain, index: number) => (
+          <div key={index} className="mb-5">
+            <h3 className="font-bold text-center">{techDomain.name}</h3>
+            {techDomain.tech.map((t: string, index: number) => (
+              <div key={index}>
+                <ul>
+                  <li className="text-quaternary">
+                    {"▹ "}
+                    {t}
+                  </li>
+                </ul>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Technologies;
