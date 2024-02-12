7680473ab50b82b2e53e7eda53a66ac8d6511c30
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-11 18:12:01 -0600

    :iphone: Changed px and py as screen scales

--- src/components/Container/UIContainer.tsx
+++ src/components/Container/UIContainer.tsx
@@ -1,9 +1,5 @@
 function UIContainer({ children }: { children: React.ReactNode }): JSX.Element {
-  return (
-    <div className="px-10 py-5 sm:px-20 sm:py-10 md:px-30 md:py-20">
-      {children}
-    </div>
-  );
+  return <div className="px-10 py-5 sm:px-20 sm:py-10">{children}</div>;
 }
 
 export default UIContainer;
