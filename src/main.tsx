5e262d004f99b4fd298f35f2c40ada100d098ce7
Kevin Nguyen <ktn1234@yahoo.com>
2024-02-11 22:18:15 -0600

    :recycle: registerSW immediately after react render

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { registerSW } from "virtual:pwa-register";
 import App from "./App.tsx";
 import "./index.css";
 
-registerSW({ immediate: true });
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+registerSW({ immediate: true });
