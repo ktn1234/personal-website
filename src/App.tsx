45e9f9121b5512ea44b23217d4e73ac5a96f1aa8
Kevin Nguyen <ktn1234@yahoo.com>
2024-06-15 17:27:06 -0500

    :sparkles: Wrapped app in BrowserRouter and mapped root path to HomePage and all other paths to NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
